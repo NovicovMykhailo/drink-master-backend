a2befb0f0ec77489499221073e5a4528c7e08095
Ievgennii <simonovievgenii@gmail.com>
2023-08-25 17:22:35 +0300

    second working version

--- controllers/subscribe.js
+++ controllers/subscribe.js
@@ -1,9 +1,21 @@
 import ctrlWrapper from "./ctrlWrapper.js";
 import sendEmail from "../helpers/sendEmail.js";
+import { HttpError } from "../helpers/HttpError.js";
 import { Subscribe } from "../models/subscribe.js";
 
 const subscribe = async (req, res) => {
 	const { email } = req.body;
+
+	const subscription = await Subscribe.findOne({ email });
+
+	if (subscription) {
+		throw HttpError(409, "Already subscribed to this email");
+	}
+
+	await Subscribe.create({
+		...req.body,
+		subscribe: true,
+	});
 
 	const subscribeEmail = {
 		to: email,
