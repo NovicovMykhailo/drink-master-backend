631bd46143e25dd8246cb00df52a60e58559ed0d
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-08-26 15:41:34 +0300

    cloudinary

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -1,6 +1,4 @@
-// import { v2 as cloudinary } from 'cloudinary';
-import cloud from "cloudinary";
-const cloudinary = cloud.v2;
+import { v2 as cloudinary } from 'cloudinary';
 
 const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = process.env;
 
