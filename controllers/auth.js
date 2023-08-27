ab239caa3ec813a4743de1c89ee8c34171458688
NovicovMykhailo <mikenovicov@gmail.com>
2023-08-27 20:40:55 +0300

    fix login path

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,7 +47,7 @@ const login = async (req, res) => {
 
   const payload = { id: user._id };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "365d" });
-  await User.findByIdAndUpdate(user._id, { token });
+  const {avatarURL} = await User.findByIdAndUpdate(user._id, { token });
 
   res.status(200).json({
     token: token,
@@ -55,6 +55,7 @@ const login = async (req, res) => {
       id: user._id,
       email: user.email,
       name: user.name,
+      avatarURL
     },
   });
 };
