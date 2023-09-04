6e7d432a4f145e1b4031f361df4e2afb09ffdb8c
NovicovMykhailo <mikenovicov@gmail.com>
2023-09-04 13:34:06 +0300

    fix glasslist

--- models/cocktail.js
+++ models/cocktail.js
@@ -1,53 +1,27 @@
 import { Schema, model } from "mongoose";
 import Joi from "joi";
 import handleMongooseError from "../helpers/handleMongoosError.js";
-const categoryList = [
-  "Ordinary Drink",
-  "Cocktail",
-  "Snake",
-  "Other/Unknow",
-  "Cocoa",
-  "Shot",
-  "Coffee/Tea",
-  "Other/Unknow",
-  "Homemade Liqueur",
-  "Punch / Party Drink",
-  "Beer",
-  "Soft Drink",
-];
-
-const glassList = [
-  "Highball glass",
-  "Cocktail glass",
-  "Old-fashioned",
-  "Whiskey Glass",
-  "Collins glass",
-  "Pousse cafe glass",
-  "Champagne flute",
-  "Whiskey sour glass",
-  "Cordial glass",
-  "Brandy snifter",
-  "White wine glass",
-];
+import { categoryList } from "../contentValues/calegories.js";
+import { glassList } from "../contentValues/glasses.js";
 
 const CocktailSchema = new Schema(
   {
     drink: { type: String, required: [true, "Set name for contact"] },
-    drinkAlternate:{ type: String, default: null},
-    tags:{ type: String, default: null},
-    video:{ type: String, default: null},
-    IBA:{ type: String, default: null},
-    alcoholic:{ type: String, default: "Alcoholic"},
+    drinkAlternate: { type: String, default: null },
+    tags: { type: String, default: null },
+    video: { type: String, default: null },
+    IBA: { type: String, default: null },
+    alcoholic: { type: String, default: "Alcoholic" },
     category: { type: String, enum: categoryList, required: [true, "Set Category"] },
     glass: { type: String, enum: glassList, required: [true, "Set Glass Type"] },
     instructions: { type: String, required: [true, "Set Instructions"] },
-    instructionsES: { type: String, default: null},
-    instructionsDE: { type: String, default: null},
-    instructionsFR: { type: String, default: null},
-    instructionsIT: { type: String, default: null},
-    instructionsRU: { type: String, default: null},
-    instructionsPL: { type: String, default: null},
-    instructionsUK: { type: String, default: null},
+    instructionsES: { type: String, default: null },
+    instructionsDE: { type: String, default: null },
+    instructionsFR: { type: String, default: null },
+    instructionsIT: { type: String, default: null },
+    instructionsRU: { type: String, default: null },
+    instructionsPL: { type: String, default: null },
+    instructionsUK: { type: String, default: null },
     ingredients: { type: Array, required: [true, "Add Ingredient"] },
     favs: { type: Array, default: [] },
     drinkThumb: { type: String, required: [true, "Photo is requiered"] },
@@ -61,10 +35,12 @@ export const Cocktail = model("cocktail", CocktailSchema);
 
 export const addCocktailSchema = Joi.object({
   drink: Joi.string().required(),
-  category: Joi.string().valid(...categoryList).required(),
-  glass: Joi.string().valid(...glassList).required(),
+  category: Joi.string()
+    .valid(...categoryList)
+    .required(),
+  glass: Joi.string()
+    .valid(...glassList)
+    .required(),
   instructions: Joi.string().required(),
   ingredients: Joi.string().required(),
 });
-
-
